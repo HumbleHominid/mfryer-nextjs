3ea28ed8b37c2f5c57f77d4d95b9af4e17a72f55
HumbleHominid <michael.d.fryer@gmail.com>
2024-11-17 20:09:53 -0700

    added email to the footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,10 +1,21 @@
+'use client';
+
 import Image from 'next/image';
 import { Email, GitHub, Resume } from '@/app/lib/ref-links';
-import { DocumentIcon } from '@heroicons/react/24/solid';
+import { DocumentIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
+import clsx from 'clsx';
 
 export default function Footer() {
+	let showCopied = false;
+	const copyEmail = function() {
+		navigator.clipboard.writeText(Email).then(() => {
+			showCopied = true;
+		});
+	}
+
 	return (
 		<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+			{/* GitHub */}
 		  <a
 			className="flex items-center gap-2 hover:underline hover:underline-offset-4"
 			href={GitHub}
@@ -18,16 +29,26 @@ export default function Footer() {
 			  width={16}
 			  height={16}
 			/>
-			GitHub
+			<span>GitHub</span>
 		  </a>
+			{/* Resume */}
 		  <a
 			className="flex items-center gap-2 hover:underline hover:underline-offset-4"
 			href={Resume}
 			target="_blank"
 			rel="noopener noreferrer"
 		  >
-			<DocumentIcon className="w-5"/> Resume
+			<DocumentIcon className="w-5"/>
+			<span>Resume</span>
 		  </a>
+			{/* Email */}
+			<div
+			className="flex relative items-center gap-2 hover:cursor-pointer hover:underline hover:underline-offset-4"
+			onClick={copyEmail}
+			>
+				<EnvelopeIcon className="w-5"/>
+				<span className="select-all">{Email}</span>
+			</div>
 		</footer>
 	);
   }
