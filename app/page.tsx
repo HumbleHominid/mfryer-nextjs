3e7de519fd724f815e121f6c9363f6e8fa08d7d8
HumbleHominid <michael.d.fryer@gmail.com>
2024-11-18 10:02:02 -0700

    fixed formatting

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,11 @@ export default function Home() {
         <p className="mb-4 p-0">
           I am Software Developer who graduated with the class of 2019 from Montana Tech. I graduated with a Bachelor&apos;s of Science in both Software Engineering and Computer Science. As part of my graduation, I was presented with an award for <span className="italic">Outstanding Achievement in the Department of Computer Science</span> from my department head.
         </p>
-        <p className="mb-4 p-0">After graduating I worked in the games industry for several years before pursuing my passion for content creation by starting a <Link href={YouTube} className="hover:underline hover:underline-offset-4 italic" target="_blank">YouTube</Link> channel. Other hobbies include gaming, fishing, and kayaking.</p>
-        <p className="mb-4 p-0">Please checkout <Link href="/about" className="hover:underline hover:underline-offset-4 italic">About</Link> for more information about myself. The source code for this application is available on my <Link href={GitHub} className="hover:underline hover:underline-offset-4 italic" target="_blank">GitHub</Link>. My resume and email are available in the footer or in the navbar.
+        <p className="mb-4 p-0">
+          After graduating I worked in the games industry for several years before pursuing my passion for content creation by starting a <Link href={YouTube} className="hover:underline hover:underline-offset-4 italic" target="_blank">YouTube</Link> channel. Other hobbies include gaming, fishing, and kayaking.
+        </p>
+        <p className="mb-4 p-0">
+          Please checkout <Link href="/about" className="hover:underline hover:underline-offset-4 italic">About</Link> for more information about myself. The source code for this application is available on my <Link href={GitHub} className="hover:underline hover:underline-offset-4 italic" target="_blank">GitHub</Link>. My resume and email are available in the footer or in the navbar.
         </p>
       </div>
     </main>
