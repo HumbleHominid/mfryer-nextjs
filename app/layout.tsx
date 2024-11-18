b230b0fd49b5190d2055f7e5838ef90fbfb0ab75
HumbleHominid <michael.d.fryer@gmail.com>
2024-11-17 17:36:38 -0700

    changed global application structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { inter } from '@/app/ui/fonts';
 import Footer from '@/app/ui/footer';
+import Navbar from '@/app/ui/navbar';
 
 export const metadata: Metadata = {
   title:{
@@ -19,8 +20,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        {children}
-        <Footer/>
+        <div className="grid grid-rows-layout min-h-lvh justify-items-stretch">
+          <Navbar/>
+          <div className="p-4 w-11/12 md:w-3/5 justify-self-center">
+            {children}
+          </div>
+          <div className="row-start-4 my-4">
+            <Footer/>
+          </div>
+        </div>
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      gridTemplateRows: {
+        "layout": 'auto auto 1fr auto'
+      }
     },
   },
   plugins: [],
