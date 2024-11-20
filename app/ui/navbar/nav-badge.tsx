872d8d09307849d1dda50c2fc88f15cada66b6eb
HumbleHominid <michael.d.fryer@gmail.com>
2024-11-19 18:46:29 -0700

    Made the badge do a little animation when you click it.

--- app/ui/navbar/nav-badge.tsx
+++ app/ui/navbar/nav-badge.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import Image from "next/image"
+import clsx from "clsx";
+import { useState } from "react";
 
 export default function NavBadge() {
+	const [clicked, setClicked] = useState(false);
+
 	return (
 		<div className="relative">
 			<Image
@@ -9,8 +15,16 @@ export default function NavBadge() {
 				width={48}
 				height={48}
 				className="rounded-full drop-shadow-lg"
+				onClick={() => setClicked(() => true)}
 			/>
-			<div className="w-12 h-12 bg-blue-500 rounded-full absolute top-0 left-0 -z-10 transform translate-x-3 translate-y-2"></div>
+			<div
+				className={clsx(
+					"w-12 h-12 bg-blue-400 rounded-full absolute top-0 left-0 -z-10",
+					{
+						"transform translate-x-3 translate-y-2 ease-in duration-1000" : clicked
+					}
+				)}
+			/>
 		</div>
 	);
 }
--- app/ui/navbar/navbar.tsx
+++ app/ui/navbar/navbar.tsx
@@ -1,53 +1,13 @@
-import Link from "next/link";
-import MFLogo from "@/app/ui/MFLogo";
-import NavLinks from "@/app/ui/navbar/nav-links";
-import { GitHub, Resume, YouTube } from "@/app/lib/ref-links";
-import NavImage from "@/app/ui/navbar/nav-image";
-import { DocumentIcon } from '@heroicons/react/24/solid';
-import NavSocial from "@/app/ui/navbar/nav-social";
+import NavBadge from "@/app/ui/navbar/nav-badge";
+import NavMail from "@/app/ui/navbar/nav-mail";
 
 export default function Navbar() {
 	return (
-		<div className="h-15 flex justify-between w-full bg-white text-gray-800 text-sm font-medium px-4 py-3 sm:py-1 sticky top-0">
-			{/* Left Section */}
-			<div className="flex justify-start gap-4">
-				<Link
-					className="hidden sm:flex items-end justify-start rounded-md self-center"
-					href="/"
-				>
-					<MFLogo />
-				</Link>
-				<div className="flex gap-4">
-					<NavLinks/>
-				</div>
+		<div className="flex justify-between w-full text-gray-800 text-sm font-medium sticky top-0">
+			<div className="m-4">
+				<NavBadge />
 			</div>
-			{/* Right Section */}
-			<div className="flex justify-end gap-4">
-				{/* GitHub */}
-				<NavSocial
-					href={GitHub}
-				>
-					<NavImage
-						src="/github-mark.svg"
-						alt="GitHub icon"
-					/>
-				</NavSocial>
-				{/* Resume */}
-				<NavSocial
-					href={Resume}
-				>
-					<DocumentIcon className="w-5"/>
-				</NavSocial>
-				{/* YouTube */}
-				<NavSocial
-					href={YouTube}
-				>
-					<NavImage
-						src="/youtube_social_icon_dark.png"
-						alt="YouTube icon"
-					/>
-				</NavSocial>
-			</div>
+			<NavMail />
 		</div>
 	)
 }
