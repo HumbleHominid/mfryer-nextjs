6f62e394669537e8c854efce245ee96c1ffb3950
HumbleHominid <michael.d.fryer@gmail.com>
2024-11-26 16:41:06 -0700

    refactored the 'game logic' such that it exists at this point out of game-canvas

---
+++ app/lib/game/snake-game.ts
@@ -0,0 +1,38 @@
+import Starfield from "@/app/lib/game/snake-game";
+
+/**
+ * The actual snake game
+ */
+export default class SnakeGame {
+	width: number;
+	height: number;
+
+	starfield: Starfield;
+
+	constructor(width: number, height: number, ) {
+		this.width = width;
+		this.height = height;
+		this.starfield = new Starfield(width, height);
+	}
+
+	tick() {
+		this.starfield.tick();
+	}
+
+	render(ctx: CanvasRenderingContext2D) {
+		const width = ctx.canvas.width;
+		const height = ctx.canvas.height;
+
+		// Draw background
+		ctx.clearRect(0, 0, width, height);
+
+		// Draw a dark screen
+		ctx.fillStyle = '#1c1c1c';
+		ctx.fillRect(0, 0, width, height);
+
+		// Draw stars
+		this.starfield.render(ctx);
+
+		ctx.restore();
+	}
+}
--- app/ui/game/game-canvas.tsx
+++ app/ui/game/game-canvas.tsx
@@ -1,28 +1,31 @@
 'use client';
 
-import { useEffect, useRef, useState } from "react";
-import Starfield from "@/app/lib/game/starfield";
+import { useEffect, useRef } from "react";
+import SnakeGame from "@/app/lib/game/snake-game";
 
-export default function GameCanvas({
-
-}: {
-
-}) {
+export default function GameCanvas() {
 	const ref = useRef(null);
 	let renderInterval: NodeJS.Timeout;
-	let stars: Starfield | null = null;
-
-	const getContext = (): CanvasRenderingContext2D | null => {
-		if (!ref.current) return null;
-		const get2dContext = (el: HTMLCanvasElement) => el.getContext('2d');
-		return get2dContext(ref.current);
-	}
+	let game: SnakeGame | null = null;
 
 	useEffect(() => {
+		const getContext = (): CanvasRenderingContext2D | null => {
+			if (!ref.current) return null;
+			const get2dContext = (el: HTMLCanvasElement) => el.getContext('2d');
+			return get2dContext(ref.current);
+		}
+
 		const ctx = getContext();
 		if (!ctx) return;
 
-		stars = new Starfield(ctx.canvas.width, ctx.canvas.height);
+		const render = () => {
+			const ctx = getContext();
+			if (!ctx) return;
+
+			game?.render(ctx);
+		}
+
+		game = new SnakeGame(ctx.canvas.width, ctx.canvas.height);
 		renderInterval = setInterval(() => render(), 50);
 
 		// render immediately as well
@@ -32,26 +35,6 @@ export default function GameCanvas({
 			clearInterval(renderInterval);
 		}
 	}, []);
-
-	const render = () => {
-		const ctx = getContext();
-		if (!ctx) return;
-
-		const width = ctx.canvas.width;
-		const height = ctx.canvas.height;
-
-		// Draw background
-		ctx.clearRect(0, 0, width, height);
-
-		// Draw a dark screen
-		ctx.fillStyle = '#1c1c1c';
-		ctx.fillRect(0, 0, width, height);
-
-		// Draw stars
-		stars?.render(ctx);
-
-		ctx.restore();
-	}
 
 	return (
 		<canvas
