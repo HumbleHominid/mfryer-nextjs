7638d6abc3db8377b5325873adc06a78959dca6b
HumbleHominid <michael.d.fryer@gmail.com>
2024-12-02 20:50:06 -0700

    hide the game on mobile

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import GameCanvas from "@/app/ui/game/game-canvas";
 
 /**
@@ -6,15 +8,17 @@ import GameCanvas from "@/app/ui/game/game-canvas";
  * scores. Global high scores can use IP address.
  */
 export default function Page() {
-
 	return (
 		<>
 			<h1 className="text-6xl mb-6">Snake Game - WIP</h1>
-			<div className="flex justify-center w-full bg-gradient-to-r from-transparent via-slate-900 to-transparent">
+			<div className="hidden sm:flex justify-center w-full bg-gradient-to-r from-transparent via-slate-900 to-transparent">
 				<div>
 					<GameCanvas />
 				</div>
 			</div>
+			<p className="inline sm:hidden">
+				This game is not supported on mobile. Sorry!
+			</p>
 		</>
 	);
 }
