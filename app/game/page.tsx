a3c94deb9fa03df914696ad60493d7c0515ff40c
HumbleHominid <michael.d.fryer@gmail.com>
2024-12-02 20:33:20 -0700

    removing paragrph with controls as it's built-in now

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -3,7 +3,7 @@ import GameCanvas from "@/app/ui/game/game-canvas";
 /**
  * This game is snake. We will have high scores saved in both local storage
  * and also have a postgreSQL database set up so save global
- * scores. Global high scores can use mac address.
+ * scores. Global high scores can use IP address.
  */
 export default function Page() {
 
@@ -15,9 +15,6 @@ export default function Page() {
 					<GameCanvas />
 				</div>
 			</div>
-			<p className="mt-4">
-				Use arrow keys to move the snake. Refresh the page to try again! Good luck!
-			</p>
 		</>
 	);
 }
