012f1ce5f1cf89dcc6a8f3f19c6f94f15a5f01d2
Michael Fryer <michael.d.fryer@gmail.com>
2025-09-24 13:43:59 +0200

    locally hosting my resume. Including a redirect from /resume to the file itself so it's easy to share

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import { Resume } from "@/app/lib/ref-links";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: Resume,
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
