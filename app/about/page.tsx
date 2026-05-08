e4682133b9e8dc4d5c918baf5d00518c9a72b371
Michael Fryer <michael.d.fryer@gmail.com>
2026-05-08 07:58:31 +0200

    about page edits

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import clsx from "clsx";
 import { YouTube, GitHub, CV } from "@/app/lib/ref-links";
 import ArcadiaVideo from "@/app/ui/video/ArcadiaVideo";
-
 export const metadata: Metadata = {
   title: "About",
 };
@@ -48,20 +47,20 @@ export default function Page() {
         Bank, where I designed and built FRED, a full-stack inventory and
         reporting system. After graduating, I joined Heavy Iron Studios in LA as
         an Associate Game Programmer. During my tenure at Heavy Iron, I was the
-        lead UI developer on PacMan: Mega Tunnel Battle. Our game was built in
+        lead UI developer on Pac-Man: Mega Tunnel Battle. Our game was built in
         UE4 and delivered to Bandai Namco in the fall of 2020. In addition to my
         responsibilities on the game UI, I also created the Live Ops portal that
-        Bandai Namco would use once the game was shipped. For that project, I
-        chose to use Django as my front-end framework of choice. After our
-        deliverable, I was put on the Rocket League team where I briefly worked
-        on a still unreleased project. I took a planned career break after Heavy
-        Iron Studios to pursue independent projects and transition into data
-        science. Built and deployed web applications for my content creation
-        work, including the Arcadia site that aggregates and distributes video
-        content for a creator collective. Collaborated with a team on regular
-        content production. Completed self-directed study in Python, statistics,
-        and machine learning, leading to my acceptance to the M.Sc. in Applied
-        Data Science at Frankfurt School of Finance &amp; Management.
+        Bandai Namco would use once the game shipped — for that I reached for
+        Django. After we shipped, I was put on the Rocket League team where I
+        briefly worked on a still-unreleased project. I took a planned career
+        break after Heavy Iron Studios to pursue independent projects and
+        transition into data science. During that time I built and deployed web
+        applications for my content creation work, including the Arcadia site
+        that aggregates and distributes video content for a creator collective.
+        I collaborated with a team on regular content production while also
+        completing self-directed study in Python, statistics, and machine
+        learning — work that ultimately led to my acceptance into the M.Sc. in
+        Applied Data Science at Frankfurt School of Finance &amp; Management.
       </p>
       {/* Education Paragraph */}
       <div>
@@ -78,20 +77,20 @@ export default function Page() {
         <p className="sm:text-justify">
           During my schooling at Montana Tech, I served as the Secretary to my
           school&apos;s student chapter of ACM in 2017-2018 and as the President
-          in 2018-2019. Before becoming the Secretary in 2016, I volunteered to
-          work for one of the local museums in town, the Butte Science Mine, on
-          a project called Digital Signage. The project&apos;s vision is to
-          bring more interactive exploration-based digital interfaces to the
-          museum that helps children and adults learn more about a subject.
-          Helping with Digital Signage secured my club roughly $4,000 for a
-          future industry trip we took to Seattle where we visited companies
-          such as Microsoft, Valve, and a couple start-ups. As the Secretary,
-          and again as the President, I helped my department get accredited as a
-          testing site for the ICPC regionals. This meant schools from all
-          around Montana could commute to Butte to compete in the competition as
-          opposed to the next closest testing site in Colorado. Many smaller
-          schools, including ourselves, would not have been able to compete in
-          the competition if there was not a local testing site like ours.
+          in 2018-2019. Before becoming Secretary, I volunteered at one of the
+          local museums, the Butte Science Mine, on a project called Digital
+          Signage. The project&apos;s vision was to bring more interactive,
+          exploration-based digital interfaces to the museum that help children
+          and adults learn more about a subject. Helping with Digital Signage
+          secured my club roughly $4,000 for an industry trip we later took to
+          Seattle, where we visited companies such as Microsoft, Valve, and a
+          couple of startups. As the Secretary, and again as the President, I
+          helped my department get accredited as a testing site for the ICPC
+          regionals. This meant schools from all around Montana could commute to
+          Butte to compete in the competition as opposed to the next closest
+          testing site in Colorado. Many smaller schools, including ourselves,
+          would not have been able to compete if there was not a local testing
+          site like ours.
         </p>
       </div>
       {/* YouTube Paragraph */}
@@ -101,28 +100,27 @@ export default function Page() {
         </div>
         <p className="sm:text-justify">
           In my free time I love to pursue my hobbies of playing video games and
-          creating video game content. In 2021 I started creating Minecraft
+          creating video game content. In 2021, I started creating Minecraft
           content on{" "}
           <a target="_blank" rel="noreferrer noopener" href={YouTube}>
             YouTube
           </a>
-          . Over these years I have gained over 500 subscribers and have felt my
+          . Over the years I have gained over 500 subscribers and have felt my
           skills as a creator and editor grow. Starting this hobby allowed me
           the opportunity to become part of a Survival Multiplayer Server, or
           SMP. By working together with the other members of Arcadia SMP, we
           have created a wonderful world for viewers to enjoy. One of my
           favorite projects on Arcadia was creating a deckbuilder game for my
-          fellow members to play. I did everything from scratch from card
+          fellow members to play. I did everything from scratch &mdash; card
           design, card balance, and &quot;
           <span className="italic">programming</span>&quot; in Minecraft. The
-          one exception would be card art which a fellow server member helped me
-          with. When the game finally released, it was so amazing to see how
-          many of my server members became addicted to it. The theory crafting
-          about which cards go well together and players getting angry about the
-          cards that they wanted not showing up in the shop was amazing to
-          witness. Seeing this project through from start to finish was highly
-          rewarding and has given me more ideas on how to go about similar
-          projects in the future.
+          one exception was card art, which a fellow server member helped me
+          with. When the game finally released, it was incredible to watch my
+          server members get hooked on it. Watching players theory-craft about
+          card combos &mdash; or rage over their most-wanted cards not showing
+          up in the shop &mdash; was endlessly entertaining. Seeing this project
+          through from start to finish was highly rewarding and has given me
+          plenty of ideas for similar projects in the future.
         </p>
       </div>
       {/* Other Hobbies Paragraph */}
@@ -138,18 +136,17 @@ export default function Page() {
           )}
         />
         <p className="sm:text-justify">
-          Not all my hobbies are related to the computer. I also enjoy getting
+          Not all my hobbies are related to the computer. I also love getting
           outdoors, especially during the summer. Taking a drive out to
-          Polebridge, MT or floating down the tobacco are both fantastic ways to
-          spend a summer day. Or even spending time closer to home by just
-          throwing bags in the backyard. I also enjoy getting out early in the
-          morning to go do some trolling on Lake Koocanusa. In the fall of 2024
-          I caught the biggest rainbow trout I&apos;ve ever seen out on
-          Koocanusa. It weighed in around 4.5 pounds! After some time in the
-          smoker it tasted delicious as well! I&apos;m looking forward to
-          getting out a fly fishing in the spring again. Montana winters can be
-          pretty long so you have to make the most out of summer while it&apos;s
-          around!
+          Polebridge, MT or floating down the Tobacco River are both fantastic
+          ways to spend a summer day, as is throwing bags in the backyard. I
+          also enjoy getting out early in the morning to do some trolling on
+          Lake Koocanusa. In the fall of 2024, I caught the biggest rainbow
+          trout I&apos;ve ever seen out on Koocanusa &mdash; it weighed in at
+          around 4.5 pounds! After some time in the smoker it tasted delicious
+          as well. I&apos;m looking forward to getting out fly fishing in the
+          spring again. Montana winters can be pretty long, so you have to make
+          the most of summer while it&apos;s around!
         </p>
       </div>
       {/* Cabin Paragraph */}
@@ -169,17 +166,16 @@ export default function Page() {
           className="float-right mb-2 ml-4 hidden h-auto w-full rounded-sm shadow-xl sm:inline sm:w-1/4 md:w-1/4"
         />
         <p className="mb-4 sm:m-0 sm:text-justify">
-          In the fall of 2024, my parent&apos;s and I renovated the exterior
-          cabin I am currently living in. This cabin was not taken care of very
-          well by the previous owners leading to significant exterior damage.
-          The previous owners had never applied chinking to maintain the
-          exterior logs. The siding was all but completely destroyed by birds
-          trying to get at bugs in the walls. We applied chinking to all the
-          exterior logs so no bugs can get in and to help seal off the cold
-          winter. We also ripped off all of the old siding which was un-painted
-          tongue-in-grove that was just stapled to backer-board. Once the 6-week
-          late Hardieboard finally showed up, we got that up over a fresh layer
-          of Tyvek. There is still more work to be done inside but the exterior
+          In the fall of 2024, my parents and I renovated the exterior of the
+          cabin I am currently living in. The previous owners hadn&apos;t taken
+          great care of it, leading to significant exterior damage &mdash; they
+          had never applied chinking to seal the logs, and the siding was nearly
+          destroyed by birds going after bugs in the walls. We applied chinking
+          to all the exterior logs to keep out bugs and help seal against the
+          cold. We also ripped off all the old siding, which was unpainted
+          tongue-and-groove just stapled to backer board. Once the six-week-late
+          Hardie board finally showed up, we got it up over a fresh layer of
+          Tyvek. There is still more work to be done inside, but the exterior
           work has already made a huge difference!
         </p>
         <Image
@@ -214,7 +210,7 @@ export default function Page() {
         , and a download for my{" "}
         <Link
           className="hover:underline hover:underline-offset-4"
-          target="_blank"
+          target="_self"
           rel="noopener noreferrer"
           href={CV}
         >
